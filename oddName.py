a8035c9c37a7470edb113e0a89585cccc93936d6
AbelLim <abel.lim@my.jcu.edu.au>
2018-03-28 16:04:18 +0800

    Ask user for name and check for and reject blank names.

--- oddName.py
+++ oddName.py
@@ -1,1 +1,10 @@
-"""Abel Lim Wee Kiat."""
+"""Ask user for name and print every second letter."""
+
+# Ask user for name.
+user_name = str(input("Please enter your name: "))
+print(user_name)
+
+# Check if user's name is blank.
+while len(user_name.strip()) == 0:
+    user_name = str(input("Please enter your name: "))
+    print(user_name)
