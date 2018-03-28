431c652e34ffee66ff99ff27037c8d829c48b981
AbelLim <abel.lim@my.jcu.edu.au>
2018-03-28 16:07:12 +0800

    Print every letter in user's name.

--- oddName.py
+++ oddName.py
@@ -4,7 +4,11 @@
 user_name = str(input("Please enter your name: "))
 print(user_name)
 
-# Check if user's name is blank.
+# Check if user_name is blank.
 while len(user_name.strip()) == 0:
     user_name = str(input("Please enter your name: "))
     print(user_name)
+
+# Print every character in user_name
+for char in user_name:
+    print(char)
